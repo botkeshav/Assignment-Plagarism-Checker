5e2d43700be9109101d41e53b5e6f87cc377daa8
Keshav <botkeshav@gmail.com>
2025-07-02 22:00:09 +0530

    Added a new function for handwritten texts

--- Utils/handler.py
+++ Utils/handler.py
@@ -4,49 +4,56 @@
 import os
 from Utils.OCR import img_compare,extract_text
 from Utils.utilities import util
-from Utils.PDF_handler import normalize_whitespace,pdftoImg
+from Utils.PDF_handler import normalize_whitespace
+from time import sleep
 
 
-class overall:
-    def compare_pages(file_path1:str,file_path2:str):
-        """
-        Compares overall pages in PDF and return the similar pages
-        based on the two function img_compare and compare_imgratio it returns True if the pdf with the less number of pages is equal to the similar number of pages
-        """
-        similar_pages =0
-        pdf_folder1 =os.listdir(file_path1) 
-        pdf_folder2 = os.listdir(file_path2)
-        #just provide the folder name and this will return the files availbale in the folder
-        img_files1 = sorted(pdf_folder1, key=lambda x: int(x.split('.')[0]))
-        img_files2 = sorted(pdf_folder2, key=lambda x: int(x.split('.')[0]))
+def compare_pages(file_path1:str,file_path2:str):
+    """
+    Compares overall pages in PDF and return the similar pages
+    based on the two function img_compare and compare_imgratio it returns True if the pdf with the less number of pages is equal to the similar number of pages
+    """
+    similar_pages =0
+    pdf_folder1 =os.listdir(file_path1) 
+    pdf_folder2 = os.listdir(file_path2)
+    #just provide the folder name and this will return the files availbale in the folder
+    img_files1 = sorted(pdf_folder1, key=lambda x: int(x.split('.')[0]))
+    img_files2 = sorted(pdf_folder2, key=lambda x: int(x.split('.')[0]))
 
-        for img in img_files1:
-            for imgs in img_files2:
-                similarity_ratio = img_compare(f"{file_path1}//{img}",f"{file_path2}//{imgs}")
-                is_similar = util.compare_imgratio(similarity_ratio)
-                if is_similar:
-                    similar_pages = similar_pages+1
-                
+    for img in img_files1:
+        for imgs in img_files2:
+            similarity_ratio = img_compare(f"{file_path1}//{img}",f"{file_path2}//{imgs}")
+            is_similar = util.compare_imgratio(similarity_ratio)
+            if is_similar:
+                similar_pages = similar_pages+1
+            
 
-        comparable_pdf = len(pdf_folder2) if len(pdf_folder1) > len(pdf_folder2) else len(pdf_folder1) 
-                            
-        if(comparable_pdf==similar_pages):
-            return True
+    comparable_pdf = len(pdf_folder2) if len(pdf_folder1) > len(pdf_folder2) else len(pdf_folder1) 
+                        
+    if(comparable_pdf==similar_pages):
+        return True
+    else:
+        return similar_pages
+
+
+def imgTotext(pdf_name:str)->str:
+
+    """Extract the content from the handwritten pdfs and provides the whole text"""
+
+    img_path= f"ss/{pdf_name}"
+    img_dir = os.listdir(img_path)  
+    content = ""
+    for img in img_dir:
+        if os.path.exists(f"{img_path}/{img}"):
+            image_text = extract_text(f"{img_path}/{img}")
+            # print(image_text)
+            # print("*******************************")
+            content = content+image_text
+            print("Sleeping for 10 secs")
+            sleep(10)
         else:
-            return similar_pages
+            print("Skipping")
+    
+    return content
 
-
-    def handwritten(s1:str,s2:str)->str:
-
-        """Just a simple function to Return the how much two sentences are similar It is a extention of function compare_sentences in utilities package
-        """
-        
-
-        pdf1_data = normalize_whitespace(s1)
-        pdf2_data = normalize_whitespace(s2)
-
-        return util.compare_sentences(pdf1_data,pdf2_data)
-
-
-    def text_extraction(pdf:str)->None:
-        pass
+   
